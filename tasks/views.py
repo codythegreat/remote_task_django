daf1008fc8eea70bdd30cae093f3b5f1d5d28748
codythegreat <maxiecody@gmail.com>
2020-03-24 13:56:17 -0500

    break out form parsing functions from request handler

--- tasks/views.py
+++ tasks/views.py
@@ -24,64 +24,86 @@ def index(request):
     if request.user.is_authenticated:
         # On post, parse the form and return a redirect to the same page (GET method)
         if request.method == 'POST':
+
+            # forms we'll use on the index page
             createForm = CreateTaskForm(request.POST)
             completeForm = CompleteTaskForm(request.POST)
             createTeamForm = CreateTeamForm(request.POST)
             joinTeamForm = JoinTeamForm(request.POST)
             commentOnTaskForm = CommentOnTask(request.POST)
+
+            # if any form is valid, parse, then redirect back to index (GET request)
             if createForm.is_valid():
-                title = createForm.cleaned_data['title']
-                desc = createForm.cleaned_data['desc']
-                endDate = createForm.cleaned_data['endDate']
-                t = Task(title=title, description=desc, endDate=endDate, startDate=datetime.datetime.now(), createdOnDate=datetime.datetime.now(), parentUser=request.user)
-                request.user.tasksOutstanding += 1
-                request.user.save()
-                t.save()
+                parseCreateTaskForm(createForm, request.user)
                 return redirect('index')
             elif completeForm.is_valid():
-                taskToComplete = Task.objects.get(pk=completeForm.cleaned_data['taskId'])
-                taskToComplete.complete = True
-                taskToComplete.completeDate = datetime.datetime.now()
-                taskToComplete.save()
-                request.user.tasksOutstanding -= 1
-                request.user.completedTasks += 1
-                request.user.save()
+                parseCompleteTaskForm(completeForm, request.user)
                 return redirect('index')
             elif createTeamForm.is_valid():
-                name = createTeamForm.cleaned_data['name']
-                private = createTeamForm.cleaned_data['private']
-                t = Team(name=name, private=private, owner=request.user)
-                t.save()
-                t.users.set([request.user])
-                t.save()
+                parseCreateTeamForm(createTeamForm, request.user)
                 return redirect('index')
             elif joinTeamForm.is_valid():
-                teamPK = joinTeamForm.cleaned_data['teamPrimKey']
-                t = Team.objects.get(pk=teamPK)
-                if not t.private:
-                    t.users.add(request.user)
-                t.save()
+                parseJoinTeamForm(joinTeamForm, request.user)
                 return redirect('index')
             elif commentOnTaskForm.is_valid():
-                parentTask = commentOnTaskForm.cleaned_data['parentTask']
-                content = commentOnTaskForm.cleaned_data['content']
-                t = Task.objects.get(pk=parentTask)
-                c = Comment(parentUser=request.user, parentTask=t, content=content)
-                c.save()
-                t.comments.add(c)
+                parseCommentOnTaskForm(commentOnTaskForm, request.user)
                 return redirect('index')
             else:
                 return redirect('index')
 
         # on GET or redirect, simply build the context and return
         else:
-            # return the rendered html file with the context
             return render(request, 'index.html', context=buildIndexContext(request))
-
+            
     # if user not authenticated, redirect to login page
     else:
         return redirect('login')
     
+# parse the create task form
+def parseCreateTaskForm(form, user):
+    title = form.cleaned_data['title']
+    desc = form.cleaned_data['desc']
+    endDate = form.cleaned_data['endDate']
+    t = Task(title=title, description=desc, endDate=endDate, startDate=datetime.datetime.now(), createdOnDate=datetime.datetime.now(), parentUser=user)
+    user.tasksOutstanding += 1
+    user.save()
+    t.save()
+
+# parse the complete task form
+def parseCompleteTaskForm(form, user):
+    taskToComplete = Task.objects.get(pk=form.cleaned_data['taskId'])
+    taskToComplete.complete = True
+    taskToComplete.completeDate = datetime.datetime.now()
+    taskToComplete.save()
+    user.tasksOutstanding -= 1
+    user.completedTasks += 1
+    user.save()
+
+# parse the create team form
+def parseCreateTeamForm(form, user):
+    name = form.cleaned_data['name']
+    private = form.cleaned_data['private']
+    t = Team(name=name, private=private, owner=user)
+    t.save()
+    t.users.set([user])
+    t.save()
+
+# parse the join team form
+def parseJoinTeamForm(form, user):
+    teamPK = form.cleaned_data['teamPrimKey']
+    t = Team.objects.get(pk=teamPK)
+    if not t.private:
+        t.users.add(user)
+    t.save()
+
+# parse comment on task form
+def parseCommentOnTaskForm(form, user):
+    parentTask = form.cleaned_data['parentTask']
+    content = form.cleaned_data['content']
+    t = Task.objects.get(pk=parentTask)
+    c = Comment(parentUser=user, parentTask=t, content=content)
+    c.save()
+    t.comments.add(c)
 
 # Search function for adding a team
 def getTeamQuerySet(query=None):
@@ -95,6 +117,7 @@ def getTeamQuerySet(query=None):
             querySet.append(team)
     return list(set(querySet))
 
+# based on request, builds and returns a context for index page
 def buildIndexContext(request):
     tasks = Task.objects.filter(parentUser=request.user)
     tasksIncomplete = len(Task.objects.filter(parentUser=request.user, complete=False).order_by("-startDate"))
