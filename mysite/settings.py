3de49083aec02cd5954c350873e1195a85596b34
codythegreat <maxiecody@gmail.com>
2020-03-24 17:05:12 -0500

    added SECRET_KEY to environ

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hy(4j*868!^-9q(9o!!gs--7h25_ai4y7b($b63e41im286)*i'
+# SECRET_KEY = 'hy(4j*868!^-9q(9o!!gs--7h25_ai4y7b($b63e41im286)*i'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['remotetasks.herokuapp.com']
 
 
 # Application definition
