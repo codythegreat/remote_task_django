e08e69e15b69209ffd0dfb8345bbe65d3d43e557
codythegreat <maxiecody@gmail.com>
2020-03-24 19:19:41 -0500

    added django-heroku pkg to project

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,4 +132,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Tell djando to use our custom user model
 AUTH_USER_MODEL = 'users.CustomUser'
 
-
+django_heroku.settings(locals())
